a5a02281b67fd85d0fa18282091f26b62dcec79f
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-11-22 10:37:27 +0900

    オプションによる分岐追記と、新規作成の実装

--- 04.class/memo3.js
+++ 04.class/memo3.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env nodememoFile
 const fs = require('fs')
 const { Select } = require('enquirer')
 const argv = require('minimist')(process.argv.slice(2))
@@ -6,18 +7,36 @@ function main () {
   const memoFile = new MemoFile()
   if (argv.l) {
     memoFile.showMemos()
+  } else if (argv.r) {
+    memoFile.insideMemos()
+  } else if (argv.d) {
+    memoFile.deleteMemo()
+  } else {
+    const input = fs.readFileSync('/dev/stdin', 'utf8')
+    memoFile.addMemo(input)
   }
 }
-
 class MemoFile {
   constructor () {
     this.memos = JSON.parse(fs.readFileSync('./memos.json', 'utf8'))
   }
 
+  addMemo (input) {
+    console.log(input)
+    const memos = this.memos
+    console.log(input)
+
+    const newMemo = {
+      title: input,
+      content: input
+    }
+
+    memos.push(newMemo)
+    fs.writeFileSync('./memos.json', JSON.stringify(memos))
+  }
+
   showMemos () {
-    this.memos.forEach(memo => {
-      console.log(memo.title)
-    })
+    this.memos.forEach(memo => { console.log(memo.title) })
   }
 }
 
